59d6042241db29d94c8947a05c9eda8441175134
Timothy Tao <timctao@gmail.com>
2016-11-07 18:41:09 -0800

    Respace helper methods

--- app/helpers/session_helper.rb
+++ app/helpers/session_helper.rb
@@ -1,6 +1,5 @@
 helpers do
 
-
   def current_user
     @current_user ||= User.find(session[:id]) if session[:id]
   end
@@ -16,4 +15,5 @@ helpers do
   def logout
     session[:id] = nil
   end
+  
 end
